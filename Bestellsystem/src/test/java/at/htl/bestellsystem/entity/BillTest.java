0b730af1f90190a88906f45cb651e0837e24fd43
AbdlAl <abdubed2@gmail.com>
2023-05-21 12:46:48 +0200

    Finished Task 23rest-33: create simpleCunstructor test for getters/setters

--- Bestellsystem/src/test/java/at/htl/bestellsystem/entity/BillTest.java
+++ Bestellsystem/src/test/java/at/htl/bestellsystem/entity/BillTest.java
@@ -1,5 +1,7 @@
 package at.htl.bestellsystem.entity;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +13,16 @@ import static org.assertj.db.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BillTest {
+
+    @BeforeEach
+    public void setUp(){
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
     @Test
     void createBill() {
         Service service = new Service("John","King");
@@ -21,4 +33,40 @@ class BillTest {
         assertThat(bill.getDesk()).isEqualTo(desk);
         assertThat(bill.getDesk().getService()).isEqualTo(service);
     }
+
+    @Test
+    void simpleConstructorGetters(){
+        //arrange
+        Service service = new Service("firstName", "lastName");
+        Desk desk = new Desk(service);
+        Bill bill = new Bill(desk, service);
+
+        //act
+
+        //assert
+        assertThat(bill.getId()).isNull();
+        assertThat(bill.getService().getFirstName()).isEqualTo("firstName");
+        assertThat(bill.getService().getLastName()).isEqualTo("lastName");
+
+    }
+
+    @Test
+    void simpleConstructorSetters(){
+        //arrange
+        Service service = new Service("Abdul", "Bedo");
+        Desk desk = new Desk(service);
+        Bill bill = new Bill(desk, service);
+
+        //act
+        bill.setId(65L);
+        bill.getService().setFirstName("Abdullah");
+        bill.getService().setLastName("Aldesoky");
+
+        //assert
+        assertThat(bill.getId()).isEqualTo(65L);
+        assertThat(bill.getService().getFirstName()).isEqualTo("Abdullah");
+        assertThat(bill.getService().getLastName()).isEqualTo("Aldesoky");
+
+    }
+
 }
