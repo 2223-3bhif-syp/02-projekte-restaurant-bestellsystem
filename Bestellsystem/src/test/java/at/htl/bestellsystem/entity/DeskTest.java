423a834e8fe3053418b7bccc2fd6b2bcd3dbf338
AbdlAl <abdubed2@gmail.com>
2023-05-21 13:08:46 +0200

    Finished Task 23rest-36: create simpleConstructor test for getters/setters

--- Bestellsystem/src/test/java/at/htl/bestellsystem/entity/DeskTest.java
+++ Bestellsystem/src/test/java/at/htl/bestellsystem/entity/DeskTest.java
@@ -1,17 +1,54 @@
 package at.htl.bestellsystem.entity;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeskTest {
+
+    @BeforeEach
+    public void setUp(){
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
     @Test
-    void createDesk() {
-        Service service = new Service("John","King");
+    void simpleConstructorGetters(){
+        //arrange
+        Service service = new Service("Johny", "Macler");
         Desk desk = new Desk(service);
 
-        assertThat(desk.getId()).isEqualTo(null);
+        //act
+
+        //assert
+        assertThat(desk.getId()).isNull();
         assertThat(desk.getService()).isEqualTo(service);
+        assertThat(desk.getService().getFirstName()).isEqualTo("Johny");
+        assertThat(desk.getService().getLastName()).isEqualTo("Macler");
+
+    }
+
+    @Test
+    void simpleConstructorSetters(){
+        //arrange
+        Service service = new Service("Johny", "Macler");
+        Desk desk = new Desk(service);
+
+        //act
+        desk.setId(34907L);
+        Service newService = new Service("anna", "hoifer");
+        desk.setService(newService);
+
+        //assert
+        assertThat(desk.getId()).isEqualTo(34907L);
+        assertThat(desk.getService()).isEqualTo(newService);
+        assertThat(desk.getService().getFirstName()).isNotEqualTo("Johny");
     }
 }
