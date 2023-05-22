f40967b4a92fe1516c5b7609d4854ddead61bfc1
AbdlAl <abdubed2@gmail.com>
2023-05-22 15:14:09 +0200

    Finished Task 23rest-46: create simpleConstructor test for getters/setters

--- Bestellsystem/src/test/java/at/htl/bestellsystem/entity/InvoiceTest.java
+++ Bestellsystem/src/test/java/at/htl/bestellsystem/entity/InvoiceTest.java
@@ -19,4 +19,50 @@ class InvoiceTest {
         assertThat(invoice.getBill()).isEqualTo(bill);
         assertThat(invoice.getProduct()).isEqualTo(product);
     }
+
+    @Test
+    void simpleConstructorGetters(){
+        //arrange
+        Service service = new Service("Markus","Müller");
+        Desk desk = new Desk(service);
+        Bill bill = new Bill(desk,service);
+        Product product = new Product("Bier",2.5, new Dish("Getränke"));
+        Invoice invoice = new Invoice(16.4,6,bill,product);
+
+
+        //act
+
+        //assert
+        assertThat(invoice.getId()).isNull();
+        assertThat(invoice.getAmoutOfItem()).isEqualTo(6);
+        assertThat(invoice.getPrice()).isEqualTo(16.4);
+        assertThat(invoice.getBill()).isEqualTo(bill);
+        assertThat(invoice.getProduct().getPrice()).isEqualTo(2.5);
+        assertThat(invoice.getProduct().getName()).isEqualTo("Bier");
+        assertThat(invoice.getProduct().getDish().getName()).isEqualTo("Getränke");
+    }
+
+    @Test
+    void simpleConstructorSetters(){
+        //arrange
+        Service service = new Service("Bertl","Bolasch");
+        Desk desk = new Desk(service);
+        Bill bill = new Bill(desk,service);
+        Product product = new Product("Fanta",1.75, new Dish("Getränke"));
+        Invoice invoice = new Invoice(5,3,bill,product);
+
+        //act
+        invoice.setId(94L);
+        invoice.setPrice(5.5);
+        invoice.getBill().getDesk().getService().setFirstName("Bolint");
+        invoice.getProduct().setName("Cola");
+
+        //assert
+        assertThat(invoice.getId()).isEqualTo(94L);
+        assertThat(invoice.getPrice()).isEqualTo(5.5);
+        assertThat(invoice.getProduct().getDish().getName()).isEqualTo("Getränke");
+        assertThat(invoice.getProduct().getName()).isEqualTo("Cola");
+        assertThat(invoice.getBill().getDesk().getService().getFirstName()).isEqualTo("Bolint");
+        assertThat(invoice.getBill().getDesk().getService().getLastName()).isEqualTo("Bolasch");
+    }
 }
