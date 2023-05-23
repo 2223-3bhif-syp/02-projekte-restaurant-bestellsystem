566bc84fbc05cf86950647b942547db8b13521a3
Armin1503 <arminsalkovic1503@gmail.com>
2023-05-23 20:06:15 +0200

    23rest-48 created default and simple constructor for getter and setter
    - added simple constructor for getter and setter
    
    - added default constructor for getter and setter

--- Bestellsystem/src/test/java/at/htl/bestellsystem/entity/ServiceTest.java
+++ Bestellsystem/src/test/java/at/htl/bestellsystem/entity/ServiceTest.java
@@ -14,4 +14,62 @@ class ServiceTest {
         assertThat(service.getFirstName()).isEqualTo("John");
         assertThat(service.getLastName()).isEqualTo("King");
     }
+
+    @Test
+    void simpleConstructorGetters(){
+        //arrange
+        Service service = new Service("John","King");
+
+        //act
+
+        //assert
+        assertThat(service.getFirstName()).isEqualTo("John");
+        assertThat(service.getLastName()).isEqualTo("King");
+
+    }
+
+    @Test
+    void simpleConstructorSetters(){
+        //arrange
+        Service service = new Service("John","King");
+
+        //act
+        service.setId(2L);
+        service.setFirstName("Abdullah");
+        service.setLastName("AlDesoky");
+
+        //assert
+        assertThat(service.getId()).isEqualTo(2L);
+        assertThat(service.getFirstName()).isEqualTo("Abdullah");
+        assertThat(service.getLastName()).isEqualTo("AlDesoky");
+    }
+
+    @Test
+    void defaultConstructorGetters(){
+        //arrange
+        Service service = new Service();
+
+        //act
+
+        //assert
+        assertThat(service.getId()).isNull();
+        assertThat(service.getFirstName()).isNull();
+        assertThat(service.getLastName()).isNull();
+    }
+
+    @Test
+    void defaultConstructorSetters(){
+        //arrange
+        Service service = new Service();
+
+        //act
+        service.setId(2L);
+        service.setFirstName("Abdullah");
+        service.setLastName("AlDesoky");
+
+        //assert
+        assertThat(service.getId()).isEqualTo(2L);
+        assertThat(service.getFirstName()).isEqualTo("Abdullah");
+        assertThat(service.getLastName()).isEqualTo("AlDesoky");
+    }
 }
