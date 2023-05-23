e2abf9829ef35d7e5ce437c9bb384c1338479ccb
Armin1503 <arminsalkovic1503@gmail.com>
2023-05-23 20:12:03 +0200

    23rest-45 created simple constructor tests for getter and setter
    - added simple constructor tests for getter and setter

--- Bestellsystem/src/test/java/at/htl/bestellsystem/entity/ProductTest.java
+++ Bestellsystem/src/test/java/at/htl/bestellsystem/entity/ProductTest.java
@@ -15,4 +15,39 @@ class ProductTest {
         assertThat(product.getName()).isEqualTo("Zwiebelsuppe");
         assertThat(product.getDish()).isEqualTo(dish);
     }
+
+    @Test
+    void simpleConstructorGetters(){
+        //arrange
+        Dish dish = new Dish("Vorspeise");
+        Product product = new Product("Zwiebelsuppe",5.60,dish);
+
+        //act
+
+        //assert
+        assertThat(product.getId()).isEqualTo(null);
+        assertThat(product.getPrice()).isEqualTo(5.60);
+        assertThat(product.getName()).isEqualTo("Zwiebelsuppe");
+        assertThat(product.getDish()).isEqualTo(dish);
+    }
+
+    @Test
+    void simpleConstructorSetters(){
+        //arrange
+        Dish dish = new Dish("Vorspeise");
+        Dish dish2 = new Dish("Nachspeise");
+        Product product = new Product("Zwiebelsuppe",5.60,dish);
+
+        //act
+        product.setId(2L);
+        product.setName("Kuchen");
+        product.setPrice(5.5);
+        product.setDish(dish2);
+
+        //assert
+        assertThat(product.getId()).isEqualTo(2L);
+        assertThat(product.getPrice()).isEqualTo(5.5);
+        assertThat(product.getName()).isEqualTo("Kuchen");
+        assertThat(product.getDish()).isEqualTo(dish2);
+    }
 }
