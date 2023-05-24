54e18720ee13cc0fbef850bdd4e163531fbfdfc3
BalintnB4 <balazsbalint513@gmail.com>
2023-05-24 09:59:34 +0200

    23rest-58 finished task
    - created module-info
    
    - created controller and view controller
    
    - added dependencies for javaFX

---
+++ Bestellsystem/src/main/java/at/htl/bestellsystem/view/App.java
@@ -0,0 +1,4 @@
+package at.htl.bestellsystem.view;
+
+public class App {
+}
---
+++ Bestellsystem/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module Bestellsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires org.apache.derby.client;
+    requires org.apache.derby.commons;
+    requires org.apache.derby.tools;
+    requires org.mybatis;
+
+    opens at.htl.survey.view to javafx.fxml;
+    exports at.htl.bestellsystem.view;
+    exports at.htl.bestellsystem.controller;
+    exports at.htl.bestellsystem.entity;
+}
