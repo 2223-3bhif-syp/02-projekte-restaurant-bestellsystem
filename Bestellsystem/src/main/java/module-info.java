85159ce77d972285dcab90ca541a74a815d2936f
BalintnB4 <balazsbalint513@gmail.com>
2023-05-24 10:35:17 +0200

    23rest-60 task not finished
    - made App
    
    - beginn with menu-fxml
    
    - made changes in module-info but still not working

--- Bestellsystem/src/main/java/module-info.java
+++ Bestellsystem/src/main/java/module-info.java
@@ -7,7 +7,7 @@ module Bestellsystem {
     requires org.apache.derby.tools;
     requires org.mybatis;
 
-    opens at.htl.survey.view to javafx.fxml;
+    opens at.htl.bestellsystem.view to javafx.fxml;
     exports at.htl.bestellsystem.view;
     exports at.htl.bestellsystem.controller;
     exports at.htl.bestellsystem.entity;
