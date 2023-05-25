f6384e19ccad4c028a0baa215d53ce83bf85a4e4
Armin1503 <arminsalkovic1503@gmail.com>
2023-05-25 08:27:07 +0200

    23rest-64 fixed error
    - fixed error
    
    - javaFX App is now working

--- Bestellsystem/src/main/java/at/htl/bestellsystem/view/App.java
+++ Bestellsystem/src/main/java/at/htl/bestellsystem/view/App.java
@@ -2,31 +2,20 @@ package at.htl.bestellsystem.view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
-
-
-public class App extends  Application {
-    private static Scene scene;
-
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("menu"), 330, 480);
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Menu");
         stage.setScene(scene);
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
