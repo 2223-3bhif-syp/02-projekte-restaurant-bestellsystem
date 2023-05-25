5a7f2786b7e5db1da120e1a1b02876092675e129
AbdlAl <abdubed2@gmail.com>
2023-05-25 15:01:20 +0200

    Task 23rest-61
    - fxml File connected with controller File
    
    - Connected the buttons and created the methods in the controller

--- Bestellsystem/src/main/java/at/htl/bestellsystem/view/MenuController.java
+++ Bestellsystem/src/main/java/at/htl/bestellsystem/view/MenuController.java
@@ -1,4 +1,43 @@
 package at.htl.bestellsystem.view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.sql.SQLException;
+
 public class MenuController {
+
+    @FXML
+    public Button foodButton;
+
+    @FXML
+    public Button dessertsButton;
+
+    @FXML
+    public Button myCartButton;
+
+    @FXML
+    public Button drinksButton;
+
+    @FXML
+    private void initialize() throws SQLException {
+
+    }
+
+    public void onClickFoodButton(ActionEvent actionEvent) {
+        System.out.println("Food Button Clicked!");
+    }
+
+    public void onClickDessertsButton(ActionEvent actionEvent) {
+        System.out.println("Desserts Button Clicked!");
+    }
+
+    public void onClickMyCartButton(ActionEvent actionEvent) {
+        System.out.println("My Cart Button Clicked!");
+    }
+
+    public void onClickDrinksButton(ActionEvent actionEvent) {
+        System.out.println("Drinks Button Clicked!");
+    }
 }
